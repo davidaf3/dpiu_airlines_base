5834bd88ea7467527ba4cd971ae2bab08a0735cb
davidaf3 <david.alvarez.fidalgo@gmail.com>
2022-10-28 17:20:15 +0200

    Añadido componenete de búsqueda de vuelos

--- src/App.js
+++ src/App.js
@@ -1,76 +1,78 @@
-import React from 'react';
-import { createClient } from '@supabase/supabase-js'
+import React from "react";
+import { createClient } from "@supabase/supabase-js";
 
-import LoginForm from './LoginForm';
-import { Route, Routes, Link, Navigate } from "react-router-dom"
+import LoginForm from "./LoginForm";
+import { Route, Routes, Link, Navigate } from "react-router-dom";
 
-import FlightDetails from './Components/FlightDetails';
-import withRouter from './Components/withRouter';
+import FlightDetails from "./Components/FlightDetails";
+import withRouter from "./Components/withRouter";
+import SearchFlight from "./Components/SearchFlight";
 
 class App extends React.Component {
-
   constructor(props) {
-    super(props)
+    super(props);
 
     // opcional para poder personalizar diferentes aspectos
     const options = {
-      schema: 'public',
-      headers: { 'x-my-custom-header': 'my-app-name' },
+      schema: "public",
+      headers: { "x-my-custom-header": "my-app-name" },
       autoRefreshToken: true,
       persistSession: true,
-      detectSessionInUrl: true
-    }
+      detectSessionInUrl: true,
+    };
 
     const supabase = createClient(
-      'https://gfhyobdofzshidbbnaxf.supabase.co', 
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmaHlvYmRvZnpzaGlkYmJuYXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY4OTUwNzQsImV4cCI6MTk4MjQ3MTA3NH0.-MgTPuKPwfZ8xJbwoblznb9EZJUCxW6cFlYHvbjrCHs',
+      "https://gfhyobdofzshidbbnaxf.supabase.co",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdmaHlvYmRvZnpzaGlkYmJuYXhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjY4OTUwNzQsImV4cCI6MTk4MjQ3MTA3NH0.-MgTPuKPwfZ8xJbwoblznb9EZJUCxW6cFlYHvbjrCHs",
       options
-    )
+    );
 
     this.supabase = supabase;
   }
 
   callBackOnFinishLoginForm = async (loginUser) => {
-
-    console.log("Cambiado "+loginUser.email);
-    console.log("Cambiado "+loginUser.password);
+    console.log("Cambiado " + loginUser.email);
+    console.log("Cambiado " + loginUser.password);
 
     // signUn, Create user
     const { data, error } = await this.supabase.auth.signInWithPassword({
       email: loginUser.email,
       password: loginUser.password,
-    })
+    });
 
-    if ( error == null && data.user != null ){
+    if (error == null && data.user != null) {
       this.setState({
-        user : data.user
-      })
+        user: data.user,
+      });
     }
-    
-  }
+  };
 
   render() {
     return (
-      <div className = "App">
+      <div className="App">
         <h1>Airplanes con supabase</h1>
         <Routes>
-            <Route path="/" element={ 
-              <LoginForm callBackOnFinishLoginForm  = { this.callBackOnFinishLoginForm } /> 
-              } />
+          <Route
+            path="/"
+            element={
+              <LoginForm
+                callBackOnFinishLoginForm={this.callBackOnFinishLoginForm}
+              />
+            }
+          />
 
-            <Route path="/flights/:code" element={ 
-              <FlightDetails supabase = {this.supabase}/> 
-            } />
+          <Route
+            path="/flights/search"
+            element={<SearchFlight supabase={this.supabase} />}
+          />
 
+          <Route
+            path="/flights/:code"
+            element={<FlightDetails supabase={this.supabase} />}
+          />
         </Routes>
       </div>
     );
   }
 }
 export default withRouter(App);
-
-
-
-
-
-
---
+++ src/Components/SearchFlight.js
@@ -0,0 +1,171 @@
+import {
+  Form,
+  Select,
+  Row,
+  Button,
+  InputNumber,
+  DatePicker,
+  Col,
+  Radio,
+} from "antd";
+import { SearchOutlined, SwapOutlined } from "@ant-design/icons";
+import { useEffect, useRef, useState } from "react";
+import moment from "moment";
+
+const excludeAirport = (airportList, code) =>
+  airportList.filter((airport) => airport.code !== code);
+
+export default function SearchFlight({ supabase, values }) {
+  const [airports, setAirports] = useState([]);
+  const [origins, setOrigins] = useState([]);
+  const [destinations, setDestinations] = useState([]);
+  const [roundTrip, setRoundTrip] = useState(values?.roundTrip ?? true);
+
+  const formRef = useRef(null);
+
+  useEffect(() => {
+    supabase
+      .from("airport")
+      .select("code, name")
+      .then(({ data }) => {
+        setAirports(data);
+      });
+  }, [supabase]);
+
+  useEffect(() => {
+    if (!values) {
+      setDestinations(airports);
+      setOrigins(airports);
+      return;
+    }
+
+    setDestinations(excludeAirport(airports, values.origin));
+    setOrigins(excludeAirport(airports, values.destination));
+  }, [airports, values]);
+
+  const searchFlights = (filters) => {
+    console.log(filters);
+  };
+
+  const swapOriginDestination = () => {
+    setDestinations(origins);
+    setOrigins(destinations);
+    const destination = formRef.current.getFieldValue("destination");
+    const origin = formRef.current.getFieldValue("origin");
+    formRef.current.setFieldValue("destination", origin);
+    formRef.current.setFieldValue("origin", destination);
+  };
+
+  const filterAirportOptions = (input, option) => {
+    const inputLower = input.toLowerCase();
+    return option.children.some((child) =>
+      child.toLowerCase().includes(inputLower)
+    );
+  };
+
+  const onAirportChange = (setter) => (code) =>
+    setter(excludeAirport(airports, code));
+
+  const disabledDate = (current) => current < moment().subtract(1, "day");
+
+  return (
+    <Form
+      ref={formRef}
+      onFinish={searchFlights}
+      name="searchFlight"
+      layout="vertical"
+      initialValues={values ?? { passengers: 1, roundTrip }}
+    >
+      <Row align="bottom">
+        <Form.Item name="roundTrip">
+          <Radio.Group onChange={(e) => setRoundTrip(e.target.value)}>
+            <Radio value={true}>Ida y vuelta</Radio>
+            <Radio value={false}>Solo ida</Radio>
+          </Radio.Group>
+        </Form.Item>
+      </Row>
+      <Row align="bottom">
+        <Col span={5}>
+          <Form.Item name="origin" label="Origen">
+            <Select
+              showSearch
+              optionFilterProp="children"
+              filterOption={filterAirportOptions}
+              onChange={onAirportChange(setDestinations)}
+            >
+              {origins.map((airport) => (
+                <Select.Option
+                  key={`origin${airport.code}`}
+                  value={airport.code}
+                >
+                  {airport.code} - {airport.name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+        <Col span={1}>
+          <Form.Item>
+            <Button
+              type="secondary"
+              icon={<SwapOutlined />}
+              style={{ width: "100%" }}
+              onClick={swapOriginDestination}
+            ></Button>
+          </Form.Item>
+        </Col>
+        <Col span={5}>
+          <Form.Item name="destination" label="Destino">
+            <Select
+              showSearch
+              optionFilterProp="children"
+              filterOption={filterAirportOptions}
+              onChange={onAirportChange(setOrigins)}
+            >
+              {destinations.map((airport) => (
+                <Select.Option
+                  key={`origin${airport.code}`}
+                  value={airport.code}
+                >
+                  {airport.code} - {airport.name}
+                </Select.Option>
+              ))}
+            </Select>
+          </Form.Item>
+        </Col>
+        <Col span={8}>
+          {roundTrip ? (
+            <Form.Item name="dates" label="Fechas">
+              <DatePicker.RangePicker
+                style={{ width: "100%" }}
+                disabledDate={disabledDate}
+              ></DatePicker.RangePicker>
+            </Form.Item>
+          ) : (
+            <Form.Item name="date" label="Fecha">
+              <DatePicker
+                style={{ width: "100%" }}
+                disabledDate={disabledDate}
+              ></DatePicker>
+            </Form.Item>
+          )}
+        </Col>
+        <Col span={4}>
+          <Form.Item name="passengers" label="Nº de pasajeros">
+            <InputNumber min={1} style={{ width: "100%" }}></InputNumber>
+          </Form.Item>
+        </Col>
+        <Col span={1}>
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              icon={<SearchOutlined />}
+              style={{ width: "100%" }}
+            ></Button>
+          </Form.Item>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
